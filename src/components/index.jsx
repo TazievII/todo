ce4393ac5b77321f1b33af8d1df47e52bc6bc97d
Ilya Taziev <tazievii@gmail.com>
2021-03-03 22:50:53 +0500

    Общие изменения

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as addListButton } from './List';
+export { default as Badge } from './Badge';
+export { default as List } from './List';
+export { default as Tasks } from './Tasks';
