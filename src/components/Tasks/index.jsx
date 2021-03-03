d504cdafb466865a376412b1641de6a1357718ab
Ilya Taziev <tazievii@gmail.com>
2021-03-03 22:55:06 +0500

    Блок задач

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,30 +1,65 @@
-import React from "react";
-import editSvg from "../../assets/img/edit.svg";
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import editSvg from '../../assets/img/edit.svg';
 
 import './Tasks.scss';
 
-const Tasks = () => {
-    return (
-        <div className="tasks">
-          <h2 className="tasks__title">
-              Фронтенд
-              <img src={editSvg} alt='edit btn'/>
-            </h2>
-            <div className="tasks__items">
-                <div className="tasks__items-row">
-                <div className='checkbox'>
-                    <input id='check' type="checkbox"/>
-                    <label htmlFor="check">
-                        <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" stroke-width="1.5" 
-                        stroke-linecap="round" stroke-linejoin="round"/></svg>
-                        </label>
-                </div>
-                <input value='React JS'/>
-                </div>
-            </div>
-        </div>
-    )
+import AddTaskForm from './AddTaskForm';
+import Task from './Task';
+
+const Tasks = ({
+  list,
+  onEditTitle,
+  onAddTask,
+  onRemoveTask,
+  onEditTask,
+  onCompleteTask,
+  withoutEmpty
+}) => {
+  const editTitle = () => {
+    const newTitle = window.prompt('Название списка', list.name);
+    if (newTitle) {
+      onEditTitle(list.id, newTitle);
+      axios
+        .patch('http://localhost:3001/lists/' + list.id, {
+          name: newTitle
+        })
+        .catch(() => {
+          alert('Не удалось обновить название списка');
+        });
+    }
+  };
+
+  return (
+    <div className="tasks">
+      <Link to={`/lists/${list.id}`}>
+        <h2 style={{ color: list.color.hex }} className="tasks__title">
+          {list.name}
+          <img onClick={editTitle} src={editSvg} alt="Edit icon" />
+        </h2>
+      </Link>
+
+      <div className="tasks__items">
+        {!withoutEmpty && list.tasks && !list.tasks.length && (
+          <h2>Задачи отсутствуют</h2>
+        )}
+        {list.tasks &&
+          list.tasks.map(task => (
+            <Task
+              key={task.id}
+              list={list}
+              onEdit={onEditTask}
+              onRemove={onRemoveTask}
+              onComplete={onCompleteTask}
+              {...task}
+            />
+          ))}
+        <AddTaskForm key={list.id} list={list} onAddTask={onAddTask} />
+      </div>
+    </div>
+  );
 };
 
-export default Tasks; 
+export default Tasks;
